c836f450e41923ddede664c823c7ca2832399f82
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-15 00:40:08 +0700

    Serving Static Files

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString;
   next();
