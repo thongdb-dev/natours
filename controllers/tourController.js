c53eaf07df60dae9c0e14e03b4b2cf5238b7ebfc
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-16 22:19:45 +0700

    Making the API Better: Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 5. Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     // Execute query
     const tours = await query;
 
