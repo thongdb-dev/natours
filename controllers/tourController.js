f87a275d048caef15993adf811ea6788184cef34
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-16 13:46:08 +0700

    Refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,19 +1,6 @@
 const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
-
-exports.checkId = (req, res, next, val) => {
-  const tour = tours.find((el) => el.id === +val);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -26,44 +13,36 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   results: tours.length,
+  //   data: {
+  //     tours: tours,
+  //   },
+  // });
 };
 
 exports.getTour = (req, res) => {
   const id = +req.params.id;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour: tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
 
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
     },
-  );
+  });
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -18,39 +18,6 @@ mongoose
   })
   .then(() => {
     console.log('DB connection successful');
-  });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log('doc', doc);
-  })
-  .catch((err) => {
-    console.log('ERROR :', err);
   });
 
 const port = process.env.PORT || 3000;
