7366592cca1f2e9f16582856f28343b1e996a893
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-16 11:39:42 +0700

    Connecting Database with the Express App

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
