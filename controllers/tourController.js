fdf82b82e1994c28cac9f8ba6704e76b3189940a
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-16 20:00:46 +0700

    Making the API Better: Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach((field) => {
+      delete queryObj[field];
+    });
+
+    const tours = await Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
 
     res.status(200).json({
       status: 'success',
