cb0d81dec92c37c5cd473d98e1c126ff7e6bc0df
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-16 21:55:02 +0700

    Making the API Better: Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,8 +14,17 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
+    // 3. Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // Execute query
     const tours = await query;
 
     // const tours = await Tour.find()
