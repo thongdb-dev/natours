1b334d4b70ac4f85562babeb833e5ee2d8a49706
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-16 13:29:02 +0700

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -38,6 +38,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log('doc', doc);
+  })
+  .catch((err) => {
+    console.log('ERROR :', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
