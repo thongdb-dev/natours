ba41d6ec384a0a262c7886fbbf8ee0736b6591a8
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-09-24 20:37:47 +0700

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -89,6 +89,12 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
